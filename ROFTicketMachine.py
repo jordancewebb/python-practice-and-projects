3c99c3b3731906b46af63ed184d51e210e76b10b
Jordance Webb <jordancewebb@yahoo.com>
2019-05-24 16:27:27 -0400

    Add files via upload

---
+++ ROFTicketMachine.py
@@ -0,0 +1,13 @@
+#Makes Tickets for ROF Project.
+import PyAutoGui
+
+#This will be a constructor for the room data structure, representing a classroom on campus
+class Room:
+    def __init__(self, building, number):
+        self.building = building
+        self.number = number
+
+class Ticket:
+
+def fillForm():
+     
