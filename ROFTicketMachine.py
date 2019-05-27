3fb5282bb1078ff9272f07c132e360a4b1f81c01
Jordance Webb <jordancewebb@yahoo.com>
2019-05-27 18:07:31 -0400

    Update ROFTicketMachine.py

--- ROFTicketMachine.py
+++ ROFTicketMachine.py
@@ -1,13 +1,24 @@
 #Makes Tickets for ROF Project.
-import PyAutoGui
+import pyautogui
 
+#This dictionary will contain all the information necessary to fill out a ring of fire project ticket
+
+dict = {'Requestor': 'Joshua Brown', 'Title': 'ROF addition in' +
+Room.toString(),'Description': 'Added USB-C Dongle', 'Building Location':
+        Room.name, 'Room location': Room.number, 'Callback number' : '252-328-9830',
+        'Responsible': 'Jordance Webb',  }
 #This will be a constructor for the room data structure, representing a classroom on campus
 class Room:
     def __init__(self, building, number):
         self.building = building
         self.number = number
 
-class Ticket:
+    def toString()
+        return self.building + " " + self.number
+
 
 def fillForm():
+    relDistance = 28
+    pyautogui.click()
+        
      
