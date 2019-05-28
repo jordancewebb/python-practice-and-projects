21034e64443b59561396c80f542d045bae1040c6
Jordance Webb <jordancewebb@yahoo.com>
2019-05-28 17:02:24 -0400

    Update ROFTicketMachine.py
    Replaced the python dictionary and class holding data for the tickets with Json's  and printed them.
    Installed/Imported pyautogui, json, pyperclip, and tested the functions pyautogui.dragRel(), pyautogui.hotkey(), and pyperclip.copy(),
    Created but not yet defined new function which will record rooms form the command line and store them in what is currently planned to be a stack.
    Created a Driver

--- ROFTicketMachine.py
+++ ROFTicketMachine.py
@@ -1,24 +1,38 @@
 #Makes Tickets for ROF Project.
 import pyautogui
+import xlsxwriter 
+import json
+import pyperclip
 
 #This dictionary will contain all the information necessary to fill out a ring of fire project ticket
+#Makes Tickets for ROF Project.
 
-dict = {'Requestor': 'Joshua Brown', 'Title': 'ROF addition in' +
-Room.toString(),'Description': 'Added USB-C Dongle', 'Building Location':
-        Room.name, 'Room location': Room.number, 'Callback number' : '252-328-9830',
-        'Responsible': 'Jordance Webb',  }
-#This will be a constructor for the room data structure, representing a classroom on campus
-class Room:
-    def __init__(self, building, number):
-        self.building = building
-        self.number = number
+dJson = {'Requestor': 'Joshua Brown', 'Title': 'ROF addition in ' + rJson[Building] + ' ' + rJson[Number], 'Description': 'Added USB-C Dongle', 'Building Location':rJson[Building}, 'Room location': rJson[Number], 'Callback number' : '252-328-9830','Responsible': 'Jordance Webb',  }
+#This will be a json for the room data structure, representing a classroom on campus
+rJson = {'Building': null, 'Number': null,}
 
-    def toString()
-        return self.building + " " + self.number
-
-
+#Collects room information from command line and stores them in a stack
+def collectRoom():
+    
 def fillForm():
-    relDistance = 28
-    pyautogui.click()
-        
-     
+    #for debugging purposes, prints json contents to console
+    print (json.dumps(dJson, indent = 4))
+    print (json.dumps(dJson, indent = 4))
+
+    #test that the functions from imported classes/modules actually work
+    distance = 28
+    counter = 5
+    while(counter > 0):
+        pyautogui.dragRel(0, distance, duration = 0.2)
+        pyperclip.copy(dJson['Title'])
+        pyautogui.hotkey("ctrl", "V")
+        counter = counter - 1
+
+#Driver portion    
+def main():
+    fillForm()        
+    exit()
+
+if __name__ == "__main__":
+    main() 
+    
